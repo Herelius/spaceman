cb0208fad6367759e4b520f36811540d07e17408
Youcef Boutrig <youcefboutrig92@gmail.com>
2021-05-11 23:00:17 +0200

    Add a filter to home page

---
+++ src/components/CardUi.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function ImgMediaCard({
+  nameImg,
+  imgUrl,
+  titleImg,
+  title,
+  launcheDay,
+  wikiLink,
+}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt={nameImg}
+          height="140"
+          image={imgUrl}
+          title={titleImg}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {launcheDay}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <a href={wikiLink}>
+          <Button size="small" color="primary">
+            Learn more
+          </Button>
+        </a>
+      </CardActions>
+    </Card>
+  );
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,181 @@
+/* eslint-disable no-unused-vars */
+import './Home.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from '../components/CardUi';
+
+const Dayjs = require('dayjs');
+
 const Home = () => {
-  return <h1>Space Man : It's not the best choice, it's Space Man choice !</h1>;
+  /* Select options */
+  const years = [
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+    '2020',
+    '2021',
+  ];
+  // const isSuccess = [null, true, false];
+  // const [selectSuccess, setSelectSuccess] = useState(null);
+  const [selectYear, setSelectYear] = useState(null);
+
+  /* API results */
+  const [alllaunches, setAlllaunches] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.spacexdata.com/v4/launches')
+      .then((res) => {
+        console.log(res.data);
+        setAlllaunches(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  /* Filter */
+  /*
+  const Filter = (year) => {
+    if (year !== 'all') {
+      return alllaunches
+        .filter((launche) => {
+          return launche.date_local.includes(year);
+        })
+        .map((launche) => {
+          return (
+            <Card
+              nameImg={launche.name}
+              imgUrl={launche.links.patch.small}
+              titleImg={launche.name}
+              title={launche.name}
+              launcheDay={Dayjs(launche.date_local).format(
+                'MMMM D, YYYY h:mm A'
+              )}
+              wikiLink={launche.links.article}
+            />
+          );
+        });
+    }
+    return alllaunches.map((launche) => {
+      return (
+        <Card
+          nameImg={launche.name}
+          imgUrl={launche.links.patch.small}
+          titleImg={launche.name}
+          title={launche.name}
+          launcheDay={Dayjs(launche.date_local).format('MMMM D, YYYY h:mm A')}
+          wikiLink={launche.links.article}
+        />
+      );
+    });
+  };
+  */
+
+  return (
+    <>
+      <h1>Space Man : It's not the best choice, it's Space Man choice !</h1>
+      <div>
+        <form>
+          <h2>Search Engine</h2>
+          <div>
+            <select
+              name="Year"
+              onChange={(event) => {
+                setSelectYear(event.target.value);
+                console.log(event.target.value);
+              }}
+            >
+              <option value="" hidden>
+                -- Year --
+              </option>
+              <option value="all">All</option>
+              {years.map((year) => {
+                return (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* <div>
+            <select
+              name="Success"
+              onChange={(event) => {
+                console.log(event.target.value);
+                setSelectSuccess(event.target.value);
+              }}
+            >
+              <option value="" hidden>
+                -- Mission success --
+              </option>
+              <option value="all">All</option>
+              {isSuccess.map((success) => {
+                return (
+                  <option key={success} value={success}>
+                    {success}
+                  </option>
+                );
+              })}
+            </select>
+          </div> */}
+        </form>
+      </div>
+      <div>
+        <h2>Results</h2>
+        <div>
+          <ul className="container">
+            {/* Filter(selectYear) */}
+            {selectYear !== 'all'
+              ? alllaunches
+                  .filter((launche) => {
+                    return launche.date_local.includes(selectYear);
+                  })
+                  .map((launche) => {
+                    return (
+                      <Card
+                        nameImg={launche.name}
+                        imgUrl={launche.links.patch.small}
+                        titleImg={launche.name}
+                        title={launche.name}
+                        launcheDay={Dayjs(launche.date_local).format(
+                          'MMMM D, YYYY h:mm A'
+                        )}
+                        wikiLink={launche.links.article}
+                      />
+                    );
+                  })
+              : alllaunches.map((launche) => {
+                  return (
+                    <Card
+                      nameImg={launche.name}
+                      imgUrl={launche.links.patch.small}
+                      titleImg={launche.name}
+                      title={launche.name}
+                      launcheDay={Dayjs(launche.date_local).format(
+                        'MMMM D, YYYY h:mm A'
+                      )}
+                      wikiLink={launche.links.article}
+                    />
+                  );
+                })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
