cebe91909544886a47da3d64d7eccbddd932f112
Heranca <heranca.kamalo@gmail.com>
2021-05-11 12:05:32 +0200

    add components on home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import Package from '../components/Package';
+import Search from '../components/Search';
+
 const Home = () => {
-  return <h1>Space Man : It's not the best choice, it's Space Man choice !</h1>;
+  return (
+    <>
+      <h1>Space Man : It's not the best choice, it's Space Man choice !</h1>
+      <Search />
+      <div>
+        <Package />
+      </div>
+    </>
+  );
 };
 
 export default Home;
