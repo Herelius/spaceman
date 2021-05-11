317769ccd94b0400ae0bbe62209c13332f2cebe7
Ornella Gatto <ornellagatto7@gmail.com>
2021-05-11 13:38:55 +0200

    create package branch

--- src/components/Package.js
+++ src/components/Package.js
@@ -1,3 +1,18 @@
-export default function Package() {
-  return <div>Package</div>;
+import React from 'react';
+
+function Package() {
+  // eslint-disable-next-line react/destructuring-assignment
+  return (
+    <div>
+      <h1>Package 1</h1>
+      <img alt="package 1" />
+      <p>Prix:</p>
+      <br />
+      <p>Météo:</p>
+      <br />
+      <p>Date:</p>
+    </div>
+  );
 }
+
+export default Package;
